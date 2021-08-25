f5565655b8ccb358b323043f185fca56e0299ebd
hill peng <iampengl@163.com>
2021-08-25 23:27:02 +0800

    .

---
+++ chapter 1/1-9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define NONBLANK 'nonblank'
+//
+int main()
+{
+    int c, prev;
+
+    if (c = getchar() != EOF) {
+        if (c != ' ') {
+            putchar(c);
+        }
+        if (c == ' ') {
+            if (prev != ' ') {
+                putchar(c);
+            }
+        }
+        prev = c;
+    }
+}
